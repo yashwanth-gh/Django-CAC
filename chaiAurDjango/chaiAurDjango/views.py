7e0226086d1ede6dba776c0f81b9d961959d1c3e
Yashwanth B M <yashwanthbm36@gmail.com>
2024-05-23 23:34:55 +0530

    learnt to use templates

--- chaiAurDjango/chaiAurDjango/views.py
+++ chaiAurDjango/chaiAurDjango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("This is Home Page ;)")
+    # return HttpResponse("This is Home Page ;)")
+    return render(request,'website/index.html')
 
 def about(request):
     return HttpResponse("This is About Page ;)")
